284073cfbb10312fd855d4639a5928fbc4f2e740
pleshro <50112229+pleshro@users.noreply.github.com>
2023-09-11 09:11:29 -0500

    Fix import errors

--- keras_vggface/models.py
+++ keras_vggface/models.py
@@ -13,11 +13,11 @@ from keras.layers import Flatten, Dense, Input, GlobalAveragePooling2D, \
     GlobalMaxPooling2D, Activation, Conv2D, MaxPooling2D, BatchNormalization, \
     AveragePooling2D, Reshape, Permute, multiply
 from keras_applications.imagenet_utils import _obtain_input_shape
-from keras.utils import layer_utils
-from keras.utils.data_utils import get_file
+from keras.src.utils import layer_utils
+from keras.src.utils.data_utils import get_file
 from keras import backend as K
 from keras_vggface import utils
-from keras.utils.layer_utils import get_source_inputs
+from keras.src.utils.layer_utils import get_source_inputs
 import warnings
 from keras.models import Model
 from keras import layers
